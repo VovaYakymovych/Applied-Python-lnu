75765b3311c4f26f876664b7a4939deb8b4209e6
VovaYakymovych <yakym.volodya@gmail.com>
2025-05-10 20:02:15 +0300

    indz4

--- lab4/2.py
+++ lab4/2.py
@@ -1,14 +1,14 @@
 from scipy.integrate import quad
 import numpy as np
 
-# Підінтегральна функція
+
 def debye_integrand(x):
     return (x**3) / (np.exp(x) - 1)
 
-# Обчислення інтегралу
+
 result, error = quad(debye_integrand, 0, np.inf)
 
-# Точне значення
+
 exact = (np.pi ** 4) / 15
 
 # Вивід
--- lab4/3.py
+++ lab4/3.py
@@ -5,24 +5,23 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Завантаження нового датасету
+
 housing = fetch_california_housing()
 X = housing.data
 y = housing.target
 
-# Розділення на тренувальну і тестову вибірку
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Лінійна регресія
 model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
-# RMSE
+
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 print(f"RMSE: {rmse:.2f}")
 
-# Графік: реальні vs передбачені значення
+
 plt.figure(figsize=(8, 6))
 plt.scatter(y_test, y_pred, edgecolor='k', alpha=0.7)
 plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], 'r--')
